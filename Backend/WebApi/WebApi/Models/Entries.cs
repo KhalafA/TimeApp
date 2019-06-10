e66eb1a62d23b44fac11662caf3da5463c8fce9a
Khalaf Abdifatah <khalaf.abdifatah@hotmail.com>
2019-06-10 15:16:23 +0200

    Entries and invoice models

---
+++ Backend/WebApi/WebApi/Models/Entries.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace WebApi.Models
+{
+    public class Entries
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Title")]
+        public string EntryTitle { get; set; }
+
+        [BsonElement("StartTime")]
+        public DateTime StartTime { get; set; }
+
+        [BsonElement("FinishTime")]
+        public DateTime FinishTime { get; set; }
+    }
+}
