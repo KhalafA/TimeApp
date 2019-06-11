8784e0ed55f5506833d9e5579a9e5f54ab49d3f4
Khalaf Abdifatah <khalaf.abdifatah@hotmail.com>
2019-06-11 20:53:41 +0200

    models

---
+++ Backend/WebApi/WebApi/Models/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Invoice
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Created")]
+        public DateTime Created { get; set; }
+
+        [BsonElement("DueDate")]
+        public DateTime DueDate { get; set; }
+
+        [BsonElement("PaymentMethod")]
+        public string PaymentMethod { get; set; }
+
+        [BsonElement("Discription")]
+        public string Discription { get; set; }
+
+        [BsonElement("Note")]
+        public string Note { get; set; }
+    }
+}
---
+++ Backend/WebApi/WebApi/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Project
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Title")]
+        public string Title { get; set; }
+
+        [BsonElement("BaseRate")]
+        public decimal BaseRate { get; set; }
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; }
+
+        [BsonElement("Invoice")]
+        public Invoice Invoice { get; set; }
+
+        [BsonElement("Entries")]
+        public List<Entry> Entries { get; set; }
+    }
+}
